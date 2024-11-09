4334065c84e82628d20e85be02f245b49919a074
ricardCortez <ricardo.cortez19@gmail.com>
2024-11-09 00:04:27 -0500

    cuarto commit se agrega base de datos mysql y crea dos tablas Menu y Bookings

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'reservation'
+    'restaurant'
 ]
 
 MIDDLEWARE = [
@@ -78,8 +78,15 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'LittleLemon',
+        'USER': 'root',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Menu(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+class Booking(models.Model):
+    customer_name = models.CharField(max_length=100)
+    date = models.DateField()
+    time = models.TimeField()
+    guests = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.customer_name} - {self.date} at {self.time}"
