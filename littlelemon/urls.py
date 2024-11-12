d0fcfc8420999576cdd558c24d4caa84a7537910
ricardCortez <ricardo.cortez19@gmail.com>
2024-11-12 01:08:40 -0500

    ulitmo commit

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('restaurant/', include('restaurant.urls')),
-    path('restaurant/menu/', include('restaurant.urls')),  # Include the restaurant app's URLs
+    # path('restaurant/menu/', include('restaurant.urls')),  # Include the restaurant app's URLs
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
 ]
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('menu/', views.MenuItemsView.as_view(), name='menu_items'),  # For listing and creating menu items
     path('menu/<int:pk>/', views.SingleMenuItemView.as_view(), name='single_menu_item'),  # For retrieving, updating, or deleting a single menu item
-    path('restaurant/booking/', include(router.urls)),
+    path('api/booking/', include(router.urls)),
     path('api-token-auth/', obtain_auth_token),
 ]
