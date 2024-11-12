4585f2bca9314fda9769fcdcdc89274dbb67b349
ricardCortez <ricardo.cortez19@gmail.com>
2024-11-12 01:15:12 -0500

    correcion de urls

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -10,8 +10,8 @@ router.register(r'tables', views.BookingViewSet)
 # Add the router URLs to the urlpatterns list
 urlpatterns = [
     path('', views.index, name='index'),
-    path('menu/', views.MenuItemsView.as_view(), name='menu_items'),  # For listing and creating menu items
-    path('menu/<int:pk>/', views.SingleMenuItemView.as_view(), name='single_menu_item'),  # For retrieving, updating, or deleting a single menu item
+    path('api/menu/', views.MenuItemsView.as_view(), name='menu_items'),  # For listing and creating menu items
+    path('api/menu/<int:pk>/', views.SingleMenuItemView.as_view(), name='single_menu_item'),  # For retrieving, updating, or deleting a single menu item
     path('api/booking/', include(router.urls)),
     path('api-token-auth/', obtain_auth_token),
 ]
